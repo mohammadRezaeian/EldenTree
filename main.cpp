a46cabe25ddb898fffbc5490e5a1de53eac5b43a
Mohammad Rezaeian <mohammadrezaeian598@gmail.com>
2025-03-03 23:25:15 +0330

    fix issue
    Fatal glibc error: pthread_mutex_lock.c:130 (___pthread_mutex_lock):
    assertion failed: mutex->__data.__owner == 0
    
    This error occurs because the threads depend on the main thread
    If EventHandler is destroyed before the detached threads finish,
    they may try accessing m_mutex,
    leading to undefined behavior

--- main.cpp
+++ main.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 #include <thread>
 #include "EventHandler.h"
+#include <memory>
 
 int main() {
-    EventHandler _eventHandler;
+    auto _eventHandler = std::make_shared<EventHandler>();  // Shared pointer
 
 
-    _eventHandler.addGod(WICHGOD::AIR, [](const std::string& action) {
+    _eventHandler->addGod(WICHGOD::AIR, [](const std::string& action) {
         std::cout << "God Air Received Message: " << action << std::endl;
     });
 
-    _eventHandler.addGod(WICHGOD::WATER, [](const std::string& action) {
+    _eventHandler->addGod(WICHGOD::WATER, [](const std::string& action) {
         std::cout << "God Water Received Message: " << action << std::endl;
     });
 
-    _eventHandler.addGod(WICHGOD::EARTH, [](const std::string& action) {
+    _eventHandler->addGod(WICHGOD::EARTH, [](const std::string& action) {
         std::cout << "God EARTH Received Message: " << action << std::endl;
     });
 
-    _eventHandler.addGod(WICHGOD::WIND, [](const std::string& action) {
+    _eventHandler->addGod(WICHGOD::WIND, [](const std::string& action) {
         std::cout << "God WIND Received Message: " << action << std::endl;
     });
 
-    std::thread([&] { _eventHandler.sendMessage(WATER, "Hello from AIR");}).detach();
-    std::thread([&] { _eventHandler.sendMessage(AIR, "Hello from WATER");}).detach();
-    std::thread([&] { _eventHandler.sendMessage(EARTH, "Hello from WATER");}).detach();
-    std::thread([&] { _eventHandler.sendMessage(WIND, "Hello from WATER");}).detach();
-
-        std::cout <<"hello world";
+    std::thread([_eventHandler] { _eventHandler->sendMessage(WATER, "Hello from AIR");}).detach();
+    std::thread([_eventHandler] { _eventHandler->sendMessage(AIR, "Hello from WATER");}).detach();
+    std::thread([_eventHandler] { _eventHandler->sendMessage(EARTH, "Hello from WATER");}).detach();
+    std::thread([_eventHandler] { _eventHandler->sendMessage(WIND, "Hello from WATER");}).detach();
 
     return 0;
 }
